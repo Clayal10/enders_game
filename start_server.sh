0499afb6faf47f1bc0bbd59a85ee3ac04d044785
Clay Shumaker <clayalshu@gmail.com>
2025-09-04 08:18:16 -0700

    basic script, should improve it

--- start_server.sh
+++ start_server.sh
@@ -4,25 +4,28 @@ set -e
 
 read -p "Kill active server? (y/n): " option
 
-if [ "$option" == "y" || "$option" == "Y" ]; then
+if [[ "$option" == "y" || "$option" == "Y" ]]; then
     echo "Killing active server"
-    oldPID=$(cat pid.txt)
-    kill oldPID
+    pid=$(ps | grep enders_game | grep -o '[0-9]\+ ' | head -n 1)
+    kill "$pid"
+    rm -r ./bin
     exit
 fi
 
-echo "Building Enders Game Server"
+mkdir bin
+
+echo "Building Ender's Game Server"
 
 cd apps/server/config
-cp Config.json ../../../
+cp Config.json ../../../bin/
 
 cd ../code
 go build -o enders_game .
-mv enders_game ../../../
-cd ../../../
+mv enders_game ../../../bin/
+cd ../../../bin/
 
-pID=$(./enders_game &)
-
-echo pID > pid.txt
+echo "Starting Ender's Game Server"
+./enders_game &
 
 echo "Server successfully started"
+exit
