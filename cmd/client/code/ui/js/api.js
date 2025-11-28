aa4c7e572ef0571c01b53e6dab43549971d794e4
Clay Shumaker <clayalshu@gmail.com>
2025-11-28 09:53:42 -0800

    added some better user error handling

--- cmd/client/code/ui/js/api.js
+++ cmd/client/code/ui/js/api.js
@@ -42,56 +42,54 @@ window.addEventListener('beforeunload', (event) => {
 //  - info | general info about the game
 //  - players | Already string formatted player / monster list
 function sendConfig(){
-    try{
-        let hostname = document.getElementById("input-hostname");
-        let port = document.getElementById("input-port");
+    let hostname = document.getElementById("input-hostname");
+    let port = document.getElementById("input-port");
+    let text = document.getElementById("game-text");
+    text.innerHTML = "";
+    const cfg = {
+        "Hostname": hostname.value,
+        "Port": port.value
+    };
 
-        const cfg = {
-            "Hostname": hostname.value,
-            "Port": port.value
-        };
-
-        fetch(setupAPI, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(cfg)
-        }).then(response => {
-            if(!response.ok){
-                throw new Error("Bad Response");
-            }
-            return response.json();
-        }).then(data => {
-            if(data.id === ""){
-                throw new Error("No valid ID");
-            }
-            console.log("New client ID: ", data.id);
-            client = new Client(data.id)
-            updateGame(data);
-        });
+    fetch(setupAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(cfg)
+    }).then(response => {
+        if(!response.ok){
+            throw new Error("Bad Response");
+        }
+        return response.json();
+    }).then(data => {
+        if(data.id === ""){
+            throw new Error("No valid ID");
+        }
+        console.log("New client ID: ", data.id);
+        client = new Client(data.id)
+        updateGame(data);
         setupDisplay(); // For character input.
-    }catch(e){
-        console.error("Could not send config: ", e);
+    }).catch(err => {
+        console.error("Could not send config: ", err);
         cleanup();
-    }
+        text.innerHTML = "Invalid Hostname and or Port. Try Again.";
+    })
 }
 
 function sendTerminate(){
-    try{
-        cleanup();
-        fetch(client.terminateAPI, {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({})
-        })
-    }catch(e){
+    cleanup();
+    fetch(client.terminateAPI, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({})
+    }).catch(e => {
         console.error("Could not send terminate: ", e)
-    }
+    })
 }
 
 function autoSendStart(){
@@ -105,150 +103,138 @@ function sendStart(){
 }
 
 function sendCharacter(character){
-    try{
-        fetch(client.startAPI, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(character)
-        }).then(response => {
-            if(!response.ok){
-                handleCharacterError();
-                throw new Error("Bad Response");
-            }
-            shouldPoll = true;
-            hide("input-submit-button");
-            hideConfig();
-            revealGameInput();
-            pollUpdateEP();
-        })
-    }catch(e){
+    fetch(client.startAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(character)
+    }).then(response => {
+        if(!response.ok){
+            handleCharacterError();
+            throw new Error("Bad Response");
+        }
+        shouldPoll = true;
+        hide("input-submit-button");
+        hideConfig();
+        revealGameInput();
+        pollUpdateEP();
+    }).catch(e => {
         console.error("Could not send start: ", e);
         return
-    }
+    })
 }
 
 function sendChangeRoom(){
-    try{
-        let changeRoomElement = document.getElementById("game-input-change-room")
-        let changeRoom = {
-            roomNumber: changeRoomElement.value
-        };
-        changeRoomElement.value = "";
-        fetch(client.changeRoomAPI, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(changeRoom)
-        }).then(response =>{
-            if(!response.ok){
-                throw new Error("Bad Response");
-            }
-        })
-    }catch(e){
+    let changeRoomElement = document.getElementById("game-input-change-room")
+    let changeRoom = {
+        roomNumber: changeRoomElement.value
+    };
+    changeRoomElement.value = "";
+    fetch(client.changeRoomAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(changeRoom)
+    }).then(response =>{
+        if(!response.ok){
+            throw new Error("Bad Response");
+        }
+    }).catch(e => {
         console.error("Could not send change room: ", e);
-    }
+    })
 }
 
 function sendFight(){
-    try{
-        fetch(client.fightAPI, {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({})
-        }).then(response => {
-            if(!response.ok){
-                throw new Error("Bad Response");
-            }
-        })
-    }catch(e){
+    fetch(client.fightAPI, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({})
+    }).then(response => {
+        if(!response.ok){
+            throw new Error("Bad Response");
+        }
+    }).catch(e => {
         console.error("Could not initiate fight: ", e);
-    }
+    })
 }
 
 function sendLoot(){
-    try{
-        let lootElement = document.getElementById("game-input-loot")
-        loot = {
-            target: lootElement.value
-        }
-        lootElement.value = "";
-
-        fetch(client.lootAPI, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(loot)
-        }).then(response => {
-            if(!response.ok){
-                throw new Error("Bad Response");
-            }
-        })
-    }catch(e){
-        console.error("Could not loot target: ", e);
+    let lootElement = document.getElementById("game-input-loot")
+    loot = {
+        target: lootElement.value
     }
+    lootElement.value = "";
+
+    fetch(client.lootAPI, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(loot)
+    }).then(response => {
+        if(!response.ok){
+            throw new Error("Bad Response");
+        }
+    }).catch(e => {
+        console.error("Could not loot target: ", e);
+    })
 }
 
 function sendPVP(){
-    try{
-        let pvpElement = document.getElementById("game-input-pvp-fight");
-        let pvp = {
-            target: pvpElement.value
-        }
-        pvpElement.value = "";
-
-        fetch(client.pvpFightAPI, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(pvp)
-        }).then(response => {
-            if(!response.ok){
-                throw new Error("Bad Response");
-            }
-        })
-    }catch(e){
-        console.error("Could not pvp fight: ", e)
+    let pvpElement = document.getElementById("game-input-pvp-fight");
+    let pvp = {
+        target: pvpElement.value
     }
+    pvpElement.value = "";
+
+    fetch(client.pvpFightAPI, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(pvp)
+    }).then(response => {
+        if(!response.ok){
+            throw new Error("Bad Response");
+        }
+    }).catch(e => {
+        console.error("Could not pvp fight: ", e)
+    })
 }
 
 function sendMessage(){
-    try{
-        let msgElement = document.getElementById("game-input-message-recipient")
-        let msgMessage = document.getElementById("game-input-message")
-        let msg = {
-            recipient: msgElement.value,
-            text: msgMessage.value
-        }
-        msgElement.value = "";
-        msgMessage.value = "";
-
-        fetch(client.messageAPI, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(msg)
-        }).then(response => {
-            if(!response.ok){
-                throw new Error("Bad Response");
-            }
-        })
-    }catch(e){
-        console.error("Could not send message: ", e)
+    let msgElement = document.getElementById("game-input-message-recipient")
+    let msgMessage = document.getElementById("game-input-message")
+    let msg = {
+        recipient: msgElement.value,
+        text: msgMessage.value
     }
+    msgElement.value = "";
+    msgMessage.value = "";
+
+    fetch(client.messageAPI, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(msg)
+    }).then(response => {
+        if(!response.ok){
+            throw new Error("Bad Response");
+        }
+    }).catch(e => {
+        console.error("Could not send message: ", e)
+    })
 }
 
 var shouldPoll = true;
